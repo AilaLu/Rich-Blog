2c8b06a7af4fad78488e7f24ce4387904d20c2a7
Aila Lu <ailalutw@gmail.com>
2022-08-02 13:18:00 +0800

    _form.html.erb for new.html.erb and edit.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,48 @@
 class ArticlesController < ApplicationController
+
+  def index
+    
+  end
+  def new
+    
+  end
+  
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to "/"
-      
+      redirect_to blogs_path, notice: "已新增"
     else
-      render "blogs/new"
+      render new_blog_path
+      # render :new
     end
   end
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to blogs_path, notice: "已編輯"
+    else
+      render edit_blog_path
+      # render :edit
+    end
+  end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.delete
+    redirect_to blogs_path, notice: "已刪除"
+  end
+  
+  
+  
  
   private
   def article_params
-    params.require(:article).permit(:title, :comtent)
+    params.require(:article).permit(:title, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- get "/", to: "welcome#home"
+#  get "/", to: "welcome#home"
  get "/about", to: "pages#about"
  
  resources :blogs#, path: "yay" 把所有的路徑都換成yay
